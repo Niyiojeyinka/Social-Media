9a6d66b4a09a515792be1e5eec6bee4c7a7ca2a2
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-04 08:54:01 +0100

    added page model

---
+++ application/models/Page_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Page_model extends CI_Model {
+
+/**
+	 *Package : Page_model
+	 *@Descriptions: A class to handle the page database operation
+	 *@Author : @niyiojeyinka(twitter,fiverr,freelancer)
+	 */
+
+ public function __construct()
+{
+    parent::__construct();
+    $this->load->database();
+
+}
+
+
+}
