98544b9cb7aec28cc2620e9b6acaa3e3a5938d35
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-04 08:54:24 +0100

    added profile model

---
+++ application/models/Profile_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Profile_model extends CI_Model {
+
+/**
+	 *Package : Auth_model
+	 *@Descriptions: A class to handle the page database operation
+	 *@Author : @niyiojeyinka(twitter,fiverr,freelancer)
+	 */
+
+ public function __construct()
+{
+    parent::__construct();
+    $this->load->database();
+
+}
+
+
+}
