9a236154bd57c63ea4070b1df3bd1812bf90d8ca
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-04 12:39:58 +0100

    solved unexpected syntax error bug

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -5,7 +5,8 @@ class Page extends CI_Controller {
   public function __construct()
   {
   	parent::__construct();
-    $this->load->model(array('page_model'))
+    $this->load->model(array('page_model'));
+
 
   }/**
 	 *Package : Page
