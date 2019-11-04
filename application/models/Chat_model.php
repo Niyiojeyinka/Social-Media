33960d394698d5253b1fbefbc1b66963e93357cb
niyiojeyinka <olaniyiojeyinka@gmail.com>
2019-11-04 08:52:40 +0100

    added Chat model

---
+++ application/models/Chat_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Chat_model extends CI_Model {
+
+/**
+	 *Package : Chat_model
+	 *@Descriptions: A class to handle the chat database operation
+	 *@Author : @niyiojeyinka(twitter,fiverr,freelancer)
+	 */
+
+ public function __construct()
+{
+    parent::__construct();
+    $this->load->database();
+
+}
+
+
+}
